0b65d0d6d5482c57f6b7cad6fb9cd1f60d1e72ca
shiyan <shiyan@hortorgames.com>
2019-03-21 20:35:12 +0800

    temp

--- common/errors/errors.go
+++ common/errors/errors.go
@@ -1,9 +1,10 @@
 package errors
 
 import (
-
+	"github.com/shiyan123/marvel.sy/common/rd"
 )
 
 var (
-
+	RespErrorOK      = rd.RespWriteErrorWithCode(0, "ok")
+	ErrInvalidParams = rd.RespWriteErrorWithCode(10000, "参数错误")
 )
